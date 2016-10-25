7632d55def60663298d2c4062871573fdaed914b
Forkhammer <forkhammer@gmail.com>
2016-10-25 20:43:44 +0500

    Проверка заполнения настроек

--- django_pushall/api.py
+++ django_pushall/api.py
@@ -26,6 +26,18 @@ class PushallAPI(object):
         else:
             return None
 
+    def _check_self(self):
+        if not PUSHALL_USER_ID:
+            raise Exception('Не указан PUSHALL_USER_ID')
+        if not PUSHALL_USER_KEY:
+            raise Exception('Не указан PUSHALL_USER_KEY')
+
+    def _check_canal(self):
+        if not PUSHALL_CANAL_ID:
+            raise Exception('Задайте PUSHALL_CANAL_ID')
+        if not PUSHALL_API_KEY:
+            raise Exception('Задайте PUSHALL_API_KEY')
+
     def self(self, title, text, url=None, icon=None, ttl=None):
         """
         Send notice to myself
@@ -36,10 +48,7 @@ class PushallAPI(object):
         :param ttl:
         :return:
         """
-        if not PUSHALL_USER_ID:
-            raise Exception('Не указан PUSHALL_USER_ID')
-        if not PUSHALL_USER_KEY:
-            raise Exception('Не указан PUSHALL_USER_KEY')
+        self._check_self()
         return self._send('self', PUSHALL_USER_ID, PUSHALL_USER_KEY, title, text, url, icon, ttl)
 
     def broadcast(self, title, text, url=None, icon=None, ttl=None):
@@ -52,6 +61,7 @@ class PushallAPI(object):
         :param ttl:
         :return:
         """
+        self._check_canal()
         return self._send('broadcast', PUSHALL_CANAL_ID, PUSHALL_API_KEY, title, text, url, icon, ttl)
 
     def multicast(self, uids, title, text, url=None, icon=None, ttl=None):
@@ -65,6 +75,7 @@ class PushallAPI(object):
         :param ttl:
         :return:
         """
+        self._check_canal()
         return self._send('multicast', PUSHALL_CANAL_ID, PUSHALL_API_KEY, title, text, url, icon, ttl, uids=json.dumps(uids))
 
     def unicast(self, uid, title, text, url=None, icon=None, ttl=None):
@@ -78,6 +89,7 @@ class PushallAPI(object):
         :param ttl:
         :return:
         """
+        self._check_canal()
         return self._send('unicast', PUSHALL_CANAL_ID, PUSHALL_API_KEY, title, text, url, icon, ttl, uid=uid)
 
     def show_list(self, lid=None):
@@ -86,6 +98,7 @@ class PushallAPI(object):
         :param lid:
         :return:
         """
+        self._check_canal()
         data = {
             'type': 'showlist',
             'id': PUSHALL_CANAL_ID,
@@ -102,6 +115,7 @@ class PushallAPI(object):
         :param uid:
         :return:
         """
+        self._check_canal()
         data = {
             'type': 'showlist',
             'subtype': 'users',
